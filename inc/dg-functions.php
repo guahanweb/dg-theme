73b002564780b3f7b22cae27d3fdeab2f405ac5a
Garth Henson <grth.a.henson@disney.com>
2015-04-20 20:58:24 -0700

    Adding the admin styling and custom column display for the composers

--- inc/dg-functions.php
+++ inc/dg-functions.php
@@ -1,4 +1,5 @@
 <?php
+// Main Theme Setup
 if (!function_exists('declaringglory_setup_theme')) {
     function declaringglory_setup_theme() {
         add_image_size('composer-thumb', 120, 120, true);
@@ -13,7 +14,7 @@ if (!function_exists('declaringglory_setup_sidebars')) {
         $args = array(
             'name' => __('Front Page Sidebar'),
             'id' => 'declaringglory-sidebar-front',
-            'description' => 'Front page sidebar',
+            'description' => 'This sidebar will be shown only on the front page of the site.',
             'class' => '',
             'before_widget' => '<aside id="%1$s-2" class="widget %2$s">',
             'after_widget' => '</aside>',
@@ -26,12 +27,20 @@ if (!function_exists('declaringglory_setup_sidebars')) {
 }
 add_action('widgets_init', 'declaringglory_setup_sidebars');
 
-// Composers
-require get_template_directory() . '/inc/plugins/composers.php';
-
+// Custom JavaScript and CSS Scripts
 if (!function_exists('declaringglory_enqueue_scripts')) {
     function declaringglory_enqueue_scripts() {
         wp_enqueue_style('declaringglory', get_template_directory_uri() . '/css/declaringglory.css');
     }
 }
 add_action('wp_enqueue_scripts', 'declaringglory_enqueue_scripts');
+
+if (!function_exists('declaringglory_enqueue_admin_scripts')) {
+    function declaringglory_enqueue_admin_scripts() {
+        wp_enqueue_style('declaringglory', get_template_directory_uri() . '/css/admin.css');
+    }
+}
+add_action('admin_enqueue_scripts', 'declaringglory_enqueue_admin_scripts');
+
+// Plugins: Composers
+require get_template_directory() . '/inc/plugins/composers.php';
--- inc/plugins/composers.php
+++ inc/plugins/composers.php
@@ -1,6 +1,9 @@
 <?php
 if (!function_exists('declaringglory_create_composers')) {
     function declaringglory_create_composers() {
+        $slug = get_theme_mod('event_permalink');
+        $slug = empty($slug) ? 'composer' : $slug;
+
         register_post_type('composer', array(
             'label' => __('composers', 'declaringglory'),
             'description' => __('Composers with whom I have worked', 'declaringglory'),
@@ -21,6 +24,7 @@ if (!function_exists('declaringglory_create_composers')) {
 
             'supports' => array('title', 'editor', 'thumbnail'),
             'taxonomies' => array('composers'),
+            'rewrite' => array('slug' => $slug),
             'hierarchical' => false,
             'public' => true,
             'show_ui' => true,
@@ -37,4 +41,34 @@ if (!function_exists('declaringglory_create_composers')) {
     }
 }
 add_action('init', 'declaringglory_create_composers', 0);
-?>
+
+if (!function_exists('declaringglory_composers_table_head')) {
+    function declaringglory_composers_table_head($defaults) {
+        $columns = array();
+        foreach ($defaults as $k => $v) {
+            if ($k == 'title') {
+                $columns['portrait'] = '<div class="composer-header composer-portrait">' . __('Portrait') . '</div>';
+                $columns[$k] = '<div class="composer-header composer-name">' . __('Name') . '</div>';
+                $columns['about'] = '<div class="composer-header composer-about">' . __('About') . '</div>';
+            } elseif ($k == 'date') {
+                $columns['author'] = '<div class="composer-header composer-added">' . __('Added By') . '</div>';
+                $columns[$k] = $v;
+            } else {
+                $columns[$k] = $v;
+            }
+        }
+        return $columns;
+    }
+}
+add_filter('manage_composer_posts_columns', 'declaringglory_composers_table_head');
+
+if (!function_exists('declaringglory_composers_table_content')) {
+    function declaringglory_composers_table_content($column_name, $post_id) {
+        if ($column_name == 'portrait') {
+            echo get_the_post_thumbnail($post_id, 'composer-thumb-small');
+        } elseif ($column_name == 'about') {
+            echo get_the_content($post_id);
+        }
+    }
+}
+add_action('manage_composer_posts_custom_column', 'declaringglory_composers_table_content', 10, 2);
