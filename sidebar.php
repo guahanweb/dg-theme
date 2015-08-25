ee99e16a09180d0af4314baae1e4f038942b280c
Garth Henson <grth.a.henson@disney.com>
2015-08-25 07:38:44 -0700

    Removing sponsor section

--- sidebar.php
+++ sidebar.php
@@ -5,10 +5,9 @@
  * @package web2feel
  */
 ?>
-	<div id="secondary" class="widget-area grid_4" role="complementary">
-		<?php do_action( 'before_sidebar' ); ?>
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+    <div id="secondary" class="widget-area grid_4" role="complementary">
+        <?php do_action( 'before_sidebar' ); ?>
+        <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-		<?php endif; // end sidebar widget area ?>
-		<?php include (TEMPLATEPATH . '/sponsors.php'); ?>	
-	</div><!-- #secondary -->
+        <?php endif; // end sidebar widget area ?>
+    </div><!-- #secondary -->
