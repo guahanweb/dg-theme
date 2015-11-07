a7f5ea1ead6eb400b53f9ab6f6f8ee1a714255a8
Garth Henson <garth.a.henson@disney.com>
2015-11-07 12:16:53 -0800

    Working on taxonomy pages

--- song-mini.php
+++ song-mini.php
@@ -3,13 +3,20 @@ $users = get_users(array(
     'connected_type' => 'writer',
     'connected_items' => $post
 ));
-print_r($users);
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
         <h3 class="entry-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
         <div class="pubilsh-info">
-            <span class="author"></span>
+            <?php
+            if ($users > 0) {
+                $authors = [];
+                foreach ($users as $author) {
+                    $authors[] = sprintf('<a href="%s" class="author-link">%s</a>', get_author_posts_url($author->ID), $author->display_name);
+                }
+                printf('<span class="author">%s</span>', implode(', ', $authors));
+            ?>
+            <?php } ?>
             <span class="date"></span>
         </div>
     </header>
