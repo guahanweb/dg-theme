f70073e77269ba5df7eb8bf4eda6c5ca31662567
Garth Henson <garth.a.henson@disney.com>
2015-11-07 12:13:59 -0800

    Working on taxonomy pages

--- song-mini.php
+++ song-mini.php
@@ -1,5 +1,16 @@
+<?php
+$users = get_users(array(
+    'connected_type' => 'writer',
+    'connected_items' => $post
+));
+print_r($users);
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <h3 class="entry-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <div class="pubilsh-info">
+            <span class="author"></span>
+            <span class="date"></span>
+        </div>
     </header>
 </article>
