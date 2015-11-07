9251939ee7fe2b502dd97d955e0a9f20560651a0
Garth Henson <garth.a.henson@disney.com>
2015-11-07 12:23:59 -0800

    Working on taxonomy pages

--- song-mini.php
+++ song-mini.php
@@ -1,23 +1,8 @@
-<?php
-$users = get_users(array(
-    'connected_type' => 'writer',
-    'connected_items' => $post
-));
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
         <h3 class="entry-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h3>
-        <div class="pubilsh-info">
-            <?php
-            if ($users > 0) {
-                $authors = [];
-                foreach ($users as $author) {
-                    $authors[] = sprintf('<a href="%s" class="author-link">%s</a>', get_author_posts_url($author->ID), $author->display_name);
-                }
-                printf('<span class="author">%s</span>', implode(', ', $authors));
-            ?>
-            <?php } ?>
-            <span class="date"></span>
-        </div>
+        <div class="entry-meta">
+            <span class="author">Posted by <?php the_author(); ?> </span> <span class="clock">  <?php the_time('M - j - Y'); ?></span> <span class="comm"><?php comments_popup_link('0 Comment', '1 Comment', '% Comments'); ?></span>
+        </div><!-- .entry-meta -->
     </header>
 </article>
