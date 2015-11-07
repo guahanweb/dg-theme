36fca4f10c45932a8e9e0cd4c327de48f6abd6a7
Garth Henson <garth.a.henson@disney.com>
2015-11-07 12:07:30 -0800

    Working on taxonomy pages

--- song-mini.php
+++ song-mini.php
@@ -1,3 +1,5 @@
-<article>
-    <p>foobar</p>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
 </article>
